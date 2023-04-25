c11a93176057b5fd0c8293cc27e560a0ad05ddbb
veyseltosun <postalarim1923@gmail.com>
2023-04-25 23:38:32 +0300

    style is get started now.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Todo from "./Todo";
 import { useState } from "react";
 const style = {
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2f80ed] to-[#1cb5e0]`,
-  heading:``,
-  form:``,
-  input:``,
-  button:``,
+  container:`bg-slate-100 max-w-[500px] rounded-md shsdow-xl p-4`,
+  heading:`text-3xl font-bold text-center text-gray-800 p-2`,
+  form:`flex justify-between`,
+  input:`border p-2 w-full text-xl`,
+  button:`border p-4 ml-2 bg-purple-500`,
+  count:``,
+  
 }
 
 function App() {
@@ -30,6 +33,7 @@ function App() {
 
         })}
       </ul>
+      <p className={style.count}>You have 2 todos.</p>
      </div>
       
     </div>
