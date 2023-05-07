ba195d72b2bf31383c09b374e817dbfacdc42d89
veyseltosun <postalarim1923@gmail.com>
2023-05-07 17:14:30 +0300

    second week I can open the app righ now.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ const style = {
   heading:`text-3xl font-bold text-center text-gray-800 p-2`,
   form:`flex justify-between`,
   input:`border p-2 w-full text-xl`,
-  button:`border p-4 ml-2 bg-purple-500`,
-  count:``,
+  button:`border p-4 ml-2 bg-purple-500 text-state-100`,
+  count:`text-center p-2`,
   
 }
 
 function App() {
-  const [todos, setTodos] = useState(["clean the house", "Feed the dog."])
+  const [todos, setTodos] = useState(["Clean the house.", "Feed the dog.", "Do the workout of the day! (WODs)"])
   return (
     <div className={style.bg}>
+      
      <div className={style.container}>
       <h3 className={style.heading}>
         Todo App
